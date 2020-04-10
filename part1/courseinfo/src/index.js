4b4e485276fefe91726bd0dde9282c537abea71e
Hideki J <aledinai@gmail.com>
2020-04-10 17:51:22 -0300

    Add Part1 - Exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        parts={[
-          { name: part1, exercises: exercises1 },
-          { name: part2, exercises: exercises2 },
-          { name: part3, exercises: exercises3 },
-        ]}
+        parts={[part1, part2, part3]}
       />
-      <Total exercisesTotal={exercises1 + exercises2 + exercises3} />
+      <Total exercisesTotal={part1.exercises + part2.exercises + part3.exercises} />
       {/* <h1>{course}</h1>
       <p>{part1} {exercises1}</p>
       <p>{part2} {exercises2}</p>
