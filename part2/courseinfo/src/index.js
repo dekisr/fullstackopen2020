1d46dcb27666ceb063d66e562b4dafcfc48c11a1
Hideki J <aledinai@gmail.com>
2020-05-01 13:05:19 -0300

    Add Part2 - Exercise 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -3,30 +3,56 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of component',
-        exercises: 14,
-        id: 3
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
   return (
     <main>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </main>
   )
 }
@@ -44,9 +70,9 @@ const Course = ({ course }) => {
 const Header = ({ course }) => <h1>{course}</h1>
 const Content = ({ parts }) => (
   <div>
-    <Part name={parts[0].name} exercises={parts[0].exercises} />
-    <Part name={parts[1].name} exercises={parts[1].exercises} />
-    <Part name={parts[2].name} exercises={parts[2].exercises} />
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
   </div>
 )
 const Part = ({ name, exercises }) => (
@@ -56,14 +82,16 @@ const Part = ({ name, exercises }) => (
 )
 const Total = ({ parts }) => {
   return (
-    <p>
-      Number of exercises:{' '}
-      <strong>
-        {parts.reduce((acc, cur) => {
-          return acc + cur.exercises
-        }, 0)}
-      </strong>
-    </p>
+    <div>
+      <p>
+        Number of exercises:{' '}
+        <strong>
+          {parts.reduce((acc, cur) => {
+            return acc + cur.exercises
+          }, 0)}
+        </strong>
+      </p>
+    </div>
   )
 }
 
