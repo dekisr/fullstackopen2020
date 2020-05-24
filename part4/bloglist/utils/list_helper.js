abdbdea360d79b6292ddada9be19ac39485042c9
Hideki J <aledinai@gmail.com>
2020-05-24 01:30:54 -0300

    Add Part4 - Exercises 4.5 / Update Exercises 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,15 @@ const totalLikes = (blogs) => {
     : blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLikes = blogs.map((blog) => blog.likes).sort((a, b) => b - a)[0]
+  return mostLikes
+    ? blogs.filter((blog) => blog.likes === mostLikes)
+    : 'There is no post with likes yet.'
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
