60795f24f7e83beb413ada46eeee9ba5881a0308
Hideki J <aledinai@gmail.com>
2020-06-03 18:52:09 -0300

    Add Part5 - Exercises 5.14

--- part5/bloglist/frontend/src/tests/Blog.test.js
+++ part5/bloglist/frontend/src/tests/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+// import { prettyDOM } from '@testing-library/dom'
 import Blog from '../components/Blog'
 
 describe('<Blog />', () => {
@@ -9,23 +10,33 @@ describe('<Blog />', () => {
     author: 'Author Test',
     url: 'urltest',
     likes: 123,
+    user: { name: 'Tester' },
   }
+  let component
   const mockHandler = jest.fn() // PropTypes...
-  const component = render(
-    <Blog blog={blog} updateBlog={mockHandler} remove={mockHandler} />
-  )
-  // const blogDiv = component.container.querySelector('div')
-  const blogHeader = component.container.querySelector('h3')
-  const blogContent = component.container.querySelector('ul')
-  console.log('what?', blogContent)
+  beforeEach(() => {
+    component = render(
+      <Blog blog={blog} updateBlog={mockHandler} remove={mockHandler} />
+    )
+  })
   // component.debug()
   test('display the blog’s title', () => {
+    const blogHeader = component.container.querySelector('h3')
     expect(blogHeader).toHaveTextContent('Test Blog')
   })
   test('display the blog’s author', () => {
+    const blogHeader = component.container.querySelector('h3')
     expect(blogHeader).toHaveTextContent('Author Test')
   })
   test('does not render the url or likes', () => {
+    const blogContent = component.container.querySelector('ul')
     expect(blogContent).toBe(null)
+  })
+  test('url and likes are shown when the view button has been clicked.', () => {
+    const viewButton = component.getByText('view')
+    fireEvent.click(viewButton)
+    const blogContent = component.container.querySelector('ul')
+    expect(blogContent).toHaveTextContent('urltest')
+    expect(blogContent).toHaveTextContent('likes 123')
   })
 })
