3130d432a02afe19f645c54bbc8b95b99d529ee0
Hideki J <aledinai@gmail.com>
2020-06-04 16:29:23 -0300

    Add Part5 - Exercises 5.18

--- part5/bloglist/frontend/cypress/integration/bloglist_spec.js
+++ part5/bloglist/frontend/cypress/integration/bloglist_spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/tests/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,23 @@ describe('Blog app', function () {
     cy.get('main > form #username')
     cy.get('main > form #password')
     cy.get('main > form button').should('contain', 'login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('main > form #username').type('mluukkai')
+      cy.get('main > form #password').type('salainen')
+      cy.get('main > form button').click()
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('main > form #username').type('mluukkai')
+      cy.get('main > form #password').type('wrong')
+      cy.get('main > form button').click()
+      cy.get('.notification')
+        .should('contain', 'Wrong credentials.')
+        .and('have.css', 'color', 'rgb(255, 99, 71)')
+    })
   })
 })
