86604d2bf53fd934127ac4c7fd13932ac4b65e73
Hideki J <aledinai@gmail.com>
2020-06-06 19:26:46 -0300

    Add Part6 - Exercises 6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -13,17 +13,31 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
   }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdote = state.find((anecdote) => anecdote.id === id)
+      const updatedAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : updatedAnecdote
+      )
+    }
+    default:
+      return state
+  }
 }
 
-export default reducer
+export const voteUp = (id) => ({ type: 'VOTE', data: { id } })
+
+export default anecdoteReducer
