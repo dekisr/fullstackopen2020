8692d96feb0f4bc55c5a5fb1093bf9b64b62d35c
Hideki J <aledinai@gmail.com>
2020-06-15 20:19:02 -0300

    Add Part8 - Exercises 8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,10 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
