5910880d43700d7afda934b89f0dd7e5e42e7ee0
Hideki J <aledinai@gmail.com>
2020-06-15 20:02:08 -0300

    Add Part8 - Exercises 8.2

--- part8/library/index.js
+++ part8/library/index.js
@@ -101,6 +101,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -108,6 +109,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
+    allBooks: () => books
   },
 }
 
