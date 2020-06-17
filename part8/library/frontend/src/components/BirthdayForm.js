77cfd65336a8cbbcdbacf39ba90b945dea3fcf3d
Hideki J <aledinai@gmail.com>
2020-06-17 08:13:45 -0300

    Add Part8 - Exercises 8.12

--- part8/library/frontend/src/components/BirthdayForm.js
+++ part8/library/frontend/src/components/BirthdayForm.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries'
 
 const BirthdayForm = ({ setError }) => {
-  const [name, setName] = useState('')
+  const authors = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState('placeholder')
   const [born, setBorn] = useState('')
   const [updateAuthor, result] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
@@ -12,7 +13,7 @@ const BirthdayForm = ({ setError }) => {
   const submit = async (event) => {
     event.preventDefault()
     updateAuthor({ variables: { name, born: Number(born) } })
-    setName('')
+    setName('placeholder')
     setBorn('')
   }
 
@@ -20,19 +21,32 @@ const BirthdayForm = ({ setError }) => {
     if (result.data && result.data.editAuthor === null) {
       setError('Author not found')
     }
-  }, [result.data])
+  }, [result.data]) // eslint-disable-line
 
   return (
     <>
       <h2>Set birthday</h2>
       <form onSubmit={submit}>
-        <div>
+        <select
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        >
+          <option value="placeholder" disabled>
+            Select an Author
+          </option>
+          {!authors.loading &&
+            !!authors.data &&
+            authors.data.allAuthors.map((author) => (
+              <option value={author.name} key={author.id}>{author.name}</option>
+            ))}
+        </select>
+        {/* <div>
           {'name '}
           <input
             value={name}
             onChange={({ target }) => setName(target.value)}
           />
-        </div>
+        </div> */}
         <div>
           {'born '}
           <input
@@ -41,7 +55,7 @@ const BirthdayForm = ({ setError }) => {
             onChange={({ target }) => setBorn(target.value)}
           />
         </div>
-        <button type="submit">change number</button>
+        <button type="submit">update author</button>
       </form>
     </>
   )
