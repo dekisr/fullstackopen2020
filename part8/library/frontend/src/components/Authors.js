84aec677fbf46283a6a7a3d6f0306a8e6c485bba
Hideki J <aledinai@gmail.com>
2020-06-17 05:55:01 -0300

    Add Part8 - Exercises 8.8

---
+++ part8/library/frontend/src/components/Authors.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  } else if (authors.loading) {
+    return <div>loading...</div>
+  } else if (!authors.data) {
+    return <div>Could not fetch the data.</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
