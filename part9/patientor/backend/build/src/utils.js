b99117a3a0a71bd8c56e6c2cec558733e9e594bd
Hideki J <aledinai@gmail.com>
2020-07-05 23:58:19 -0300

    Add Part9 - Exercises 9.13

--- part9/patientor/backend/build/src/utils.js
+++ part9/patientor/backend/build/src/utils.js
@@ -1,7 +1,53 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const types_1 = require("./types");
+const isString = (text) => {
+    return typeof text === 'string' || text instanceof String;
+};
+const parseName = (name) => {
+    if (!name || !isString(name)) {
+        throw new Error(`Incorrect or missing name: ${name}`);
+    }
+    return name;
+};
+const parseSsn = (ssn) => {
+    if (!ssn || !isString(ssn)) {
+        throw new Error(`Incorrect or missing ssn: ${ssn}`);
+    }
+    return ssn;
+};
+const parseOccupation = (occupation) => {
+    if (!occupation || !isString(occupation)) {
+        throw new Error(`Incorrect or missing occupation: ${occupation}`);
+    }
+    return occupation;
+};
+const isDate = (date) => {
+    return Boolean(Date.parse(date));
+};
+const parseDateOfBirth = (date) => {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error(`Incorrect or missing date of birth: ${date}`);
+    }
+    return date;
+};
+const isGender = (param) => {
+    return Object.values(types_1.Gender).includes(param);
+};
+const parseGender = (gender) => {
+    if (!gender || !isGender(gender)) {
+        throw new Error(`Incorrect or missing gender: ${gender}`);
+    }
+    return gender;
+};
 const toNewPatient = (object) => {
-    const newPatient = Object.assign({}, object);
-    return newPatient;
+    return {
+        name: parseName(object.name),
+        dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+        ssn: parseSsn(object.ssn),
+        gender: parseGender(object.gender),
+        occupation: parseOccupation(object.occupation),
+    };
 };
 exports.default = toNewPatient;
--- part9/patientor/backend/src/utils.ts
+++ part9/patientor/backend/src/utils.ts
@@ -1,9 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NewPatient } from './types';
+import { NewPatient, Gender } from './types';
+
+const isString = (text: any): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+const parseName = (name: any): string => {
+  if (!name || !isString(name)) {
+    throw new Error(`Incorrect or missing name: ${name as string}`);
+  }
+  return name;
+};
+const parseSsn = (ssn: any): string => {
+  if (!ssn || !isString(ssn)) {
+    throw new Error(`Incorrect or missing ssn: ${ssn as string}`);
+  }
+  return ssn;
+};
+const parseOccupation = (occupation: any): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error(`Incorrect or missing occupation: ${occupation as string}`);
+  }
+  return occupation;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+const parseDateOfBirth = (date: any): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error(`Incorrect or missing date of birth: ${date as string}`);
+  }
+  return date;
+};
+
+const isGender = (param: any): param is Gender => {
+  return Object.values(Gender).includes(param);
+};
+const parseGender = (gender: any): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error(`Incorrect or missing gender: ${gender as string}`);
+  }
+  return gender;
+};
 
 const toNewPatient = (object: any): NewPatient => {
-  const newPatient: NewPatient = { ...object };
-  return newPatient;
+  return {
+    name: parseName(object.name),
+    dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+    ssn: parseSsn(object.ssn),
+    gender: parseGender(object.gender),
+    occupation: parseOccupation(object.occupation),
+  };
 };
 
 export default toNewPatient;
